57fbdd69cf1e4ac6707646925c852b9b5633f28d
vitor.dev <146399948+its-vitor@users.noreply.github.com>
2023-12-16 00:11:59 -0300

    Add files via upload

---
+++ javascript/coins.js
@@ -0,0 +1,7 @@
+let coins = 2345500;
+
+setInterval(function() {
+    let elem = document.getElementById('embed-acs');
+    coins +=  Math.floor(Math.random() * (4000 - 3000 + 1)) + 3000;
+    elem.textContent = coins + " Ac's em estoque!";
+}, 1000);
